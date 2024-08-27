9af32ca955ca82ef0a764167c30aca2c7ba07ded
ojnoah9 <110123439+ojnoah9@users.noreply.github.com>
2024-08-27 12:04:15 +0100

    Update next.config.js to allow external image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["skillicons.dev"], // Add other domains if needed
+  },
+};
 
 export default nextConfig;
