26a868208fda5ea1955138da419ee6b2fa8548e1
Noah Samuel OJIMA-OJO <110123439+ojnoah9@users.noreply.github.com>
2024-08-27 13:17:52 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "www.skillicons.dev",
-        port: "",
-        pathname: "/icons/**", // Adjust this if your path is different
-      },
-    ],
-  },
-};
 
-module.exports = nextConfig;
